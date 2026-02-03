514238d08d5c68f0a31392f8eeca441248fb4816
Miguel Masó <miguel.maso@upc.edu>
2026-02-03 11:48:42 +0100

    exploring viscous variation wrt temp

--- Calibration/thermal_variation.jl
+++ Calibration/thermal_variation.jl
@@ -12,7 +12,18 @@ g_values = g.(θ_values, 293, 45.1)
 plot(θ_values .-273, g_values)
 
 ex = data[1:3:end]
+ex = [record for record in data if record.λ_max == 2]
 
 θ_exp = [e.θ for e in ex]
 μ_avg = [e.σ_max / e.λ_max for e in ex]
-plot(θ_exp .- 273, μ_avg ./ μ_avg[3], xlabel="T [ºC]", ylabel="μ/μR [KPa]")
+g_exp = μ_avg ./ μ_avg[3]
+ann   = [text(@sprintf("%.2f",g),8,:left,:bottom) for g in g_exp]
+p=plot(θ_exp .- 273, g_exp, label="θr = 40 ºC", xlabel="T [ºC]", ylabel="g = μ / μR [-]", mark=:circle, lw=2, msw=0)
+annotate!(θ_exp .- 273, g_exp, ann)
+display(p);
+
+p=plot()
+for i in 1:length(ex)
+  plot!(ex[i].λ, ex[i].σ ./ g_exp[i], label=@sprintf("%3.0f ºC",ex[i].θ-K0), xlabel="Stretch [-]", ylabel="Stress / g(θ) [Pa]", mark=:circle, lw=2, msw=0)
+end
+display(p);
