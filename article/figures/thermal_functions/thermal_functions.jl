64e2ccda58aaeeac4dfd0b59dbfcc68348f7e602
miguelmaso <miguel.maso@upc.edu>
2026-05-18 22:32:16 +0200

    Update thermal laws plot

--- article/figures/thermal_functions/thermal_functions.jl
+++ article/figures/thermal_functions/thermal_functions.jl
@@ -1,23 +1,27 @@
 using HyperFEM
 using Plots
+using LaTeXStrings
 
 pgfplotsx()  # Comment this line or type `GR()` for a faster (non LaTeX-like) rendering
-default(palette   = :seaborn_colorblind)
-default(legend    = :topleft)
-default(linewidth = 2)
+default(palette        = :seaborn_colorblind)
+default(legend         = :topleft)
+default(linewidth      = 3)
+default(legendfontsize = 12)
+default(tickfontsize   = 12)
+default(labelfontsize  = 12)
 
 γ = 0.5
 
 law_a = EntropicElasticityLaw(θr=1.0, γ=γ)
 law_b = NonlinearMeltingLaw(θr=1.0, θM=2.0, γ=γ)
-law_c = NonlinearSofteningLaw(θr=1.0, θt=0.8, γ=4γ, δ=0.5)
+law_c = NonlinearSofteningLaw(θr=1.0, θT=0.8, γ=4γ, δ=0.5)
 
-fa, dfa, ddfa = derivatives(law_a)
-fb, dfb, ddfb = derivatives(law_b)
-fc, dfc, ddfc = derivatives(law_c)
+fa, dfa, ddfa = law_a()
+fb, dfb, ddfb = law_b()
+fc, dfc, ddfc = law_c()
 
 labels = ["Entropic elasticity" "Nonlinear melting" "Nonlinear softening"]
 
-p = plot(0:0.01:2, [fa, fb, fc], label=labels, xlabel="θ/θᵣ", ylabel="Ψ/Ψᵣ")
-savefig(abspath(@__DIR__, "..", stem(@__FILE__) * ".pdf"))
+p = plot(0:0.01:2, [fa, fb, fc], label=labels, xlabel=L"\theta / \theta_R", ylabel=L"\Psi / \Psi_R")
+savefig(abspath(dirname(@__FILE__), "..", stem(@__FILE__) * ".pdf"))
 display(p);
