0e4fdf9c980ddfd42cec36e904e2357fd615a1b9
Miguel Masó <miguel.maso@upc.edu>
2026-06-11 12:38:23 +0200

    thermal functions f softening

--- article/figures/thermal_functions/thermal_functions.jl
+++ article/figures/thermal_functions/thermal_functions.jl
@@ -2,9 +2,10 @@ using HyperFEM
 using Plots
 using LaTeXStrings
 
-pgfplotsx()  # Comment this line or type `GR()` for a faster (non LaTeX-like) rendering
+pgfplotsx()  # Comment this line or type `gr()` for a faster (non LaTeX-like) rendering
+default(fontfamily     = "Computer Modern")
 default(palette        = :seaborn_colorblind)
-default(legend         = :topleft)
+default(legend         = :top)
 default(linewidth      = 3)
 default(legendfontsize = 12)
 default(tickfontsize   = 12)
@@ -14,16 +15,13 @@ default(labelfontsize  = 12)
 
 law_a = EntropicElasticityLaw(θr=1.0, γ=γ)
 law_b = NonlinearMeltingLaw(θr=1.0, θM=2.0, γ=γ)
-law_c = NonlinearSofteningLaw(θr=1.0, θT=0.8, γ=4γ, δ=0.5)
+law_c = NonlinearSofteningLaw(θr=1.0, θT=0.8, γ=2γ, δ=0.5)
 law_d = ConstantCvLaw(θr=1.0)
-
-fa, dfa, ddfa = law_a()
-fb, dfb, ddfb = law_b()
-fc, dfc, ddfc = law_c()
-fd, dfd, ddfd = law_d()
+laws = [law_a, law_b, law_c, law_d]
+funcs = map(law -> law()[1], laws)
 
 labels = ["Entropic elasticity" "Nonlinear melting" "Nonlinear softening" "Constant cᵥ"]
 
-p = plot(0:0.01:2, [fa, fb, fc, fd], label=labels, xlabel=L"\theta / \theta_R", ylabel=L"\Psi / \Psi_R")
+p = plot(0:0.01:2, funcs, label=labels, xlabel=L"\theta / \theta_R", ylabel=L"\Psi / \Psi_R")
 # savefig(abspath(dirname(@__FILE__), "..", stem(@__FILE__) * ".pdf"))
 display(p);
