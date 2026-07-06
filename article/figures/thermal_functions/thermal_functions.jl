a7e55d0869c96d2390b25f5a92a8eb8d4a90c409
miguelmaso <miguel.maso@upc.edu>
2026-07-06 08:41:25 +0200

    thermal funcs

--- article/figures/thermal_functions/thermal_functions.jl
+++ article/figures/thermal_functions/thermal_functions.jl
@@ -20,7 +20,9 @@ law_b = NonlinearMeltingLaw(θr=1.0, θM=2.0, γ=γ)
 law_c = NonlinearSofteningLaw(θr=1.0, θT=0.8, γ=2γ, δ=0.3)
 law_d = ConstantCvLaw(θr=1.0)
 laws = [law_a, law_b, law_c, law_d]
-funcs = map(law -> law()[1], laws)
+funcs_f = map(law -> law()[1], laws)
+funcs_η = map(law -> law()[2], laws)
+funcs_G = map(df -> (θ) -> df(θ)/df(1.0), funcs_η)
 
 labels = ["Entropic elasticity" "Nonlinear melting" "Nonlinear softening" "Constant cᵥ"]
 styles = [:solid :solid :solid :dash]
@@ -29,8 +31,17 @@ N = 4
 
 p = plot(; xlabel=L"\theta / \theta_R", ylabel=L"\Psi / \Psi_R", extra_kwargs)
 for i in 1:N
-  plot!(0:0.01:2, funcs[i]; label=labels[i], style=styles[i])
+  plot!(0:0.01:2, funcs_f[i]; label=labels[i], style=styles[i])
 end
 scatter!([1],[1], color=:black, label=nothing)
-savefig(abspath(dirname(@__FILE__), "..", stem(@__FILE__) * ".pdf"))
+savefig(abspath(dirname(@__FILE__), "..", stem(@__FILE__) * "_Psi_" * string(N) * ".pdf"))
+display(p);
+
+
+p = plot(; xlabel=L"\theta / \theta_R", ylabel=L"\eta / \eta_R", extra_kwargs)
+for i in 1:N
+  plot!(0:0.01:2, funcs_G[i]; label=labels[i], style=styles[i], ylim=[-0.5, Inf])
+end
+scatter!([1],[1], color=:black, label=nothing)
+savefig(abspath(dirname(@__FILE__), "..", stem(@__FILE__) * "_eta_" * string(N) * ".pdf"))
 display(p);
